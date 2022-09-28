1598cbc0c2df201ec0d17c1b7ab718686f337636
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-28 15:03:05 -0300

    C# Explorando a linguagem Módulo 3 - Conversões de Outros Tipos

---
+++ ConversoesDeOutrosTipos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+        
+        //long é um tipo de variável de 64 bits
+        long x = 2000000000000000000;
+        Console.WriteLine(x);
+        
+        short y = 32000;
+        Console.WriteLine(y);
+        
+        float z = 1.6234f;
+        Console.WriteLine(z);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
