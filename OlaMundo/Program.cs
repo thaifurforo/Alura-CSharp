c622b338373b759feb87d3436fcc6f6ee3c50285
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-27 21:35:21 -0300

    C# Explorando a linguagem Módulo 2 - Nosso primerio programa

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo!");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
