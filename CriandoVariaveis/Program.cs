8ef17c80b4166b0911a9defe3c6780b876f7a9a9
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-28 10:01:46 -0300

    C# Explorando a linguagem Módulo 3 - variáveis numéricas (inteiros)

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int idade;
+
+        idade = 10;
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 6 * 2 - 5;
+        Console.Write("A idade de Fulano é " + idade + " anos. ");
+        
+        idade = (27 - 5) * 2;
+        Console.WriteLine("Já a Ciclana tem " + idade + " anos.");
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
