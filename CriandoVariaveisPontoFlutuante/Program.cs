37a0c19145bc86a237f68399e888f61a64a5dc57
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-28 13:48:29 -0300

    C# Explorando a linguagem Módulo 3 - Variáveis double

---
+++ CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        
+        double salario;
+        salario = 3000.13;
+        Console.WriteLine(salario);
+
+        double idade;
+        
+        idade = 7 / 5;
+        Console.WriteLine(idade);
+
+        idade = 7.0 / 5;
+        Console.WriteLine(idade);
+
+        idade = 7 / 5.0;
+        Console.WriteLine(idade);
+        
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+    
+}
