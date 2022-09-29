af55eac2b344e40129c18543852691f96050c9a9
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 10:07:23 -0300

    C# Explorando a linguagem Módulo 5 - Variável booleana

---
+++ Condicionais2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        bool teste = true;
+        
+        if (teste)
+        {
+            Console.WriteLine("Teste está ok");
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
