04050fece29d928a24e91888a422271ebcb2cab2
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 10:28:27 -0300

    C# Explorando a linguagem Módulo 5 - Escopo

---
+++ Escopo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o Projeto 9 - Escopo");
+        
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+        
+        // A variável tem que ser declarada no escopo principal pra poder ser usada dentro do próximo if, senão dará erro
+        string textoAdicional;
+
+        if (!acompanhado)
+            textoAdicional = "João não está acompanhado";
+        else
+            textoAdicional = "João está acompanhado";
+
+        Console.WriteLine(textoAdicional);
+        
+        if (idadeJoao >= 18 || acompanhado)
+            Console.WriteLine("Pode entrar!");
+        else
+            Console.WriteLine("Não pode entrar!");
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
