f455445d677458d8b94edebbba5dc731bb7c99ae
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 20:55:04 -0300

    C# Explorando a linguagem Módulo 6 - Laço de repetição for

---
+++ CalculaPoupanca2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        decimal investimento = 1000;
+        
+        // Rendimento de 0,5% a.m
+        decimal aliquota = 0.005m;
+
+        for (int mes = 1; mes <= 12; mes ++)
+        { 
+            investimento *= (1 + aliquota);
+            Console.WriteLine("O valor do investimento no mês " + mes + " é de " + investimento);
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
