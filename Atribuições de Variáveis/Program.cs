65d3f6057d8b86286df0612e862e2683cd6ea7d5
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 09:18:28 -0300

    C# Explorando a linguagem Módulo 4 - Atribuições de Variáveis

---
+++ Atribuições de Variáveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 6 - Atribuições de Variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+        
+        Console.WriteLine(idadeAna);
+        Console.WriteLine(idade);
+        
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
