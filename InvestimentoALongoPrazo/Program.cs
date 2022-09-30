e7b5762b49294c58d077360b6b49b263547bc490
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 21:35:33 -0300

    C# Explorando a linguagem Módulo 6 - Encadeamento de for

---
+++ InvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 12 - Investimento a longo prazo");
+
+        decimal fatorRendimento = 1.005m;
+        decimal investimento = 1000;
+
+        for (int anos = 1; anos <= 5; anos++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001m;
+        }
+        
+        // variável mes só existe dentro do contexto do loop for, se tentar usar ela fora vai dar erro
+        
+        Console.WriteLine("Valor final do investimento: " + investimento);
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
