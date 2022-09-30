78633060e75ee17eb8a82c0aeaa1e53b7b569387
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 21:50:08 -0300

    C# Explorando a linguagem Módulo 6 - Encadeando For

---
+++ EncadeandoFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 13 - Encadeando for");
+
+        for (int lin = 0; lin < 10; lin++)
+        {
+            for (int col = 0; col < lin; col++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
