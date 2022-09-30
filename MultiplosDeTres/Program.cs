508baa8d0adb80a9868ad7d6d431c406700616ba
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 22:06:48 -0300

    C# Explorando a linguagem Módulo 6 - Exercício For

---
+++ MultiplosDeTres/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício laço for");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+                Console.WriteLine(i);
+        }
+        
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
