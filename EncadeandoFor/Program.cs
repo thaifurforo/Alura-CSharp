4ee128ca19b54d7352923c40553ee14354ff2f3c
thainara.furforo <thainara.furforo@stone.com.br>
2022-09-29 21:52:56 -0300

    C# Explorando a linguagem Módulo 6 - Encadeando For + Break

--- EncadeandoFor/Program.cs
+++ EncadeandoFor/Program.cs
@@ -15,6 +15,19 @@ class Programa
             Console.WriteLine();
         }
 
+        Console.WriteLine("");
+        
+        for (int lin = 0; lin < 10; lin++)
+        {
+            for (int col = 0; col < lin; col++)
+            {
+                Console.Write("*");
+                if (col >= lin)
+                    break;
+            }
+            Console.WriteLine();
+        }
+        
         Console.WriteLine("Tecle enter para fechar...");
         Console.ReadLine();
     }
